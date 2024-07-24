938397963a71a7b8ce7b0ea53a25de9ba890dee5
AyushiAnand70047 <ayushianand088@gmail.com>
2024-07-24 13:58:02 +0530

    Task Manager App

---
+++ backend/Routes/TaskRouter.js
@@ -0,0 +1,16 @@
+const {createTask, fetchAllTasks, updateTaskById, deleteTaskById} = require('../Controllers/TaskController');
+const router = require('express').Router();
+
+// to get all the task
+router.get('/', fetchAllTasks);
+
+// to create a task
+router.post('/',createTask);
+
+// to update a task
+router.put('/:id',updateTaskById);
+
+// to delete a task
+router.delete('/:id',deleteTaskById);
+
+module.exports = router;
